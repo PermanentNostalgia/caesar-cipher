52ed3d21e6365a3db7f6749e8d0c0567a573fa7c
PermanentNostalgia <permanentnostal@daum.net>
2024-03-02 17:51:28 +0900

    Feat: 파일 암호화 기능으로 변경

--- main.py
+++ main.py
@@ -18,30 +18,47 @@ class StringTokens:
 
         return string
         
+def encrypt(plain, rotations):
+    plain_tokens = StringTokens(plain)
+    syllables = []
 
-plain = input("암호화할 문자열을 입력해주세요: ")
-plain_tokens = StringTokens(plain)
-syllables = []
+    for index, word in enumerate(plain):
+        if HanSyllable.isHangul(word):
+            syllables.append([index,HanSyllable(word)])
 
-for index, word in enumerate(plain):
-    if HanSyllable.isHangul(word):
-        syllables.append([index,HanSyllable(word)])
-
-rotation = int(input("회전시킬 바퀴수를 입력하세요: "))
-
-for word in syllables:
-    index = word[0]
-    syllable = word[1]
+    for word in syllables:
+        index = word[0]
+        syllable = word[1]
     
-    offsets = syllable.getOffsets()
-    offsets[2] += 1 # 종성의 오프셋의 경우 1 작게 설정되어 있음
+        offsets = syllable.getOffsets()
+        offsets[2] += 1 # 종성의 오프셋의 경우 1 작게 설정되어 있음
+        
+        offsets[0] = (offsets[0] + rotations[0]) % 19
+        offsets[1] = (offsets[1] + rotations[1]) % 21
+        offsets[2] = (offsets[2] + rotations[2]) % 28 - 1
 
-    offsets[0] = (offsets[0] + rotation) % 19
-    offsets[1] = (offsets[1] + rotation) % 21
-    offsets[2] = (offsets[2] + rotation) % 28 - 1
+        syllable.setOffsets(offsets)
 
-    syllable.setOffsets(offsets)
+        plain_tokens.editToken(index, str(syllable))
 
-    plain_tokens.editToken(index, str(syllable))
+    return plain_tokens.toStr()
 
-print(plain_tokens.toStr())
+while True:
+    name = input("암호화할 파일의 이름을 입력하세요: ")
+    enc_name = input("암호문을 저장할 파일의 이름을 입력하세요: ")
+    r1 = int(input("초성의 회전수를 입력하세요: "))
+    r2 = int(input("중성의 회전수를 입력하세요: "))
+    r3 = int(input("종성의 회전수를 입력하세요: "))
+
+    file = open(name, "r", encoding="UTF-8")
+
+    plain = file.read()
+    file.close()
+    
+    encrypted = encrypt(plain, [r1,r2,r3])
+
+    new_file = open(enc_name, "w", encoding="UTF-8")
+    new_file.write(encrypted)
+
+    new_file.close()
+    print("--암호화 성공--")
